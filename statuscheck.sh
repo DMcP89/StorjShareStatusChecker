25e94854bf329af544758b0e5d1c1d5f923e19f9
Dave <davemcpherson@wochstudios.com>
2017-07-19 00:36:53 -0400

    added DIR variable to statuscheck.sh

--- statuscheck.sh
+++ statuscheck.sh
@@ -3,10 +3,10 @@
 # Orignally written by: Dave McPherson, 2017
 # https://github.com/DMcP89/StorjShareStatusChecker
 #######################################################
-
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 now=$(date)
 log="statuscheck.log"
 echo "$now -- Collecting storj stats" > $log
 
-python bin/statuscheck.py
+python $DIR/bin/statuscheck.py
 
